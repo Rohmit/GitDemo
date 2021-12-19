05fa1a186f4a082c4727bcdaddb1f0bc78ba9557
Rohit Mahto <rohitmahto9@gmail.com>
2021-12-19 18:56:04 +0530

    merge

--- src/test/java/stepDefinations/Development.java
+++ src/test/java/stepDefinations/Development.java
@@ -4,7 +4,7 @@ public class Development {
 
 	public static void main(String[] args) {
 		System.out.println("Commit changes in Development branch");
-		
+		System.out.println("Commit all changes in master branch");
 	}
 
 }
